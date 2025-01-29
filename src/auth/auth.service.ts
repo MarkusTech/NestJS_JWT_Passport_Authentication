72c7a73a8c2fb493c8aae55525ba431b973c5062
MarkusTech <wm.recopelacion@gmail.com>
2025-01-29 16:27:30 +0800

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,40 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcryptjs';
 import { UsersService } from 'src/users/users.service';
+import { TokenPayload } from './token-payload.interface';
+import { User } from 'src/users/schema/user.schema';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const expiresAccessToken = new Date();
+    expiresAccessToken.setUTCMilliseconds(
+      expiresAccessToken.getTime() +
+        parseInt(
+          this.configService.getOrThrow<string>(
+            'JWT_ACCESS_TOKEN_EXPIRATION_MS',
+          ),
+        ),
+    );
+
+    const tokenPayload: TokenPayload = {
+      userId: user._id.toHexString(),
+    };
+    const accessToken = this.jwtService.sign(tokenPayload, {
+      secret: this.configService.getOrThrow('JWT_ACCESS_TOKEN_SECRET'),
+      expiresIn: `${this.configService.getOrThrow(
+        'JWT_ACCESS_TOKEN_EXPIRATION_MS',
+      )}`,
+    });
+  }
 
   async verifyUser(email: string, password: string): Promise<any> {
     try {
